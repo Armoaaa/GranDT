55ef3d6f33e92ee051a81b6db5b2bbd498668cdd
Armoaaa <armoaalaea@gmail.com>
2025-09-29 20:19:30 -0300

    AGREGAR DAPPER

---
+++ src/cSharp/GranDT.Dapper/AdoDapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using Dapper;
+using MySqlConnector;
+using GranDT.Core;
+
+
+
+namespace GranDT.Dapper;
+
+public class AdoDapper : IAdo
+{
+    //Defino una asociacion a un objeto que sabe conectarse a una BD.
+    private readonly IDbConnection _conexion;
+
+    public AdoDapper(IDbConnection conexion) => this._conexion = conexion;
+
+    //Este constructor usa por defecto la cadena para un conector MySQL
+    public AdoDapper(string cadena) => _conexion = new MySqlConnection(cadena);
+}
---
+++ src/cSharp/GranDT.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace GranDT.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
