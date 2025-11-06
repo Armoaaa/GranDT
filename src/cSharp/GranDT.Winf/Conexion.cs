b04c6070b8e5858823d474c12931b3d6a698c0a6
Armoaaa <164027531+Armoaaa@users.noreply.github.com>
2025-11-06 12:20:17 -0300

    arniaaa

--- src/cSharp/GranDT.Winf/Conexion.cs
+++ src/cSharp/GranDT.Winf/Conexion.cs
@@ -5,7 +5,8 @@ namespace GranDT.Winf
 {
     public static class Conexion
     {
-        private static readonly string _cadena = "Server=localhost;User ID=5to_agbd;Password=Trigg3rs!;Database=5to_GranDT;";
+        private static readonly string _cadena = "Server=localhost;User ID=root;Password=root!;Database=5to_GranDT;";
+        //private static readonly string _cadena = "Server=localhost;User ID=5to_agbd;Password=Trigg3rs!;Database=5to_GranDT;";
         private static IDbConnection? _conexion;
 
         public static IDbConnection GetConexion()
